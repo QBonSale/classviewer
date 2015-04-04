66f87d140c9a857be159f509496195aa0261d322
Owen Horwitz <owen@owenhorwitz.com>
2015-04-04 02:10:46 -0700

    running scrub_registrar script generates a list of url's to scrub

--- scrub_registrar.py
+++ scrub_registrar.py
@@ -3,7 +3,7 @@
 from selenium import webdriver
 
 
-def major_list():
+def get_major_list():
     driver = webdriver.Firefox()
     driver.get("http://www.registrar.ucla.edu/schedule/schedulehome.aspx")
     options = driver.find_element_by_id("ctl00_BodyContentPlaceHolder_SOCmain_lstSubjectArea")
@@ -13,3 +13,24 @@ def major_list():
     driver.close()
     driver.quit()
     return options_list
+
+def get_term_list():
+    term_list=[]
+    #TODO: verify this range
+    for year in range(1999,2015):
+        #TODO: add summer
+        for term in ['F','W','S']:
+            term_list.append(str(year)[2:]+term)
+    return term_list
+
+def print_url(major, term):
+    #TODO: handle spaces in the url
+    print "http://www.registrar.ucla.edu/schedule/crsredir.aspx?termsel=" + term + "&subareasel="+ major
+    return
+
+
+majors = get_major_list()
+terms = get_term_list()
+for major in majors:
+    for term in terms:
+        print_url(major,term)
